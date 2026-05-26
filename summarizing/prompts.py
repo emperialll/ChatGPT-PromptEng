22e5712ea5acaf0493b201816507eae76bdaaad8
emperialll <17363660+emperialll@users.noreply.github.com>
2026-05-26 13:32:16 +0200

    Adds prompts and the product review sample in summarizing dir

---
+++ summarizing/prompts.py
@@ -0,0 +1,54 @@
+# Version 1 - Summarize with a word/sentence/character limit
+def prompt_v1(prod_review: str) -> str:
+    return f"""
+Your task is to generate a short summary of a product \
+review from an ecommerce site. 
+
+Summarize the review below, delimited by triple 
+backticks, in at most 30 words. 
+
+Review: ```{prod_review}```
+"""
+
+# Version 2 - Summarize with a focus on shipping and delivery
+def prompt_v2(prod_review: str) -> str:
+    return f"""
+Your task is to generate a short summary of a product \
+review from an ecommerce site to give feedback to the \
+Shipping deparmtment. 
+
+Summarize the review below, delimited by triple 
+backticks, in at most 30 words, and focusing on any aspects \
+that mention shipping and delivery of the product. 
+
+Review: ```{prod_review}```
+"""
+
+# Version 3 - Summarize with a focus on price and value
+def prompt_v3(prod_review: str) -> str:
+    return f"""
+Your task is to generate a short summary of a product \
+review from an ecommerce site to give feedback to the \
+pricing deparmtment, responsible for determining the \
+price of the product.  
+
+Summarize the review below, delimited by triple 
+backticks, in at most 30 words, and focusing on any aspects \
+that are relevant to the price and perceived value. 
+
+Review: ```{prod_review}```
+"""
+
+# Version 4 - "extract" instead of "summarize"
+def prompt_v4(prod_review: str) -> str:
+    return f"""
+Your task is to extract relevant information from \ 
+a product review from an ecommerce site to give \
+feedback to the Shipping department. 
+
+From the review below, delimited by triple quotes \
+extract the information relevant to shipping and \ 
+delivery. Limit to 30 words. 
+
+Review: ```{prod_review}```
+"""
