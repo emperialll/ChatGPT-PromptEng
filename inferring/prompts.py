3a6329459563c557b1e5c3ab65309934c79ff6d8
emperialll <17363660+emperialll@users.noreply.github.com>
2026-05-27 22:02:42 +0200

    adds prompts for inferring

--- inferring/prompts.py
+++ inferring/prompts.py
@@ -4,7 +4,7 @@ def prompt_v1(lamp_review: str) -> str:
 What is the sentiment of the following product review, 
 which is delimited with triple backticks?
 
-Give your answer as a single word, either "positive" \
+Give your answer as a single word, either "positive"
 or "negative".
 
 Review text: '''{lamp_review}'''
@@ -13,9 +13,9 @@ Review text: '''{lamp_review}'''
 # Version 2 - Identify types of emotions
 def prompt_v2(lamp_review: str) -> str:
     return f"""
-Identify a list of emotions that the writer of the \
-following review is expressing. Include no more than \
-five items in the list. Format your answer as a list of \
+Identify a list of emotions that the writer of the
+following review is expressing. Include no more than
+five items in the list. Format your answer as a list of
 lower-case words separated by commas.
 
 Review text: '''{lamp_review}'''
@@ -24,8 +24,8 @@ Review text: '''{lamp_review}'''
 # Version 3 - Identify anger
 def prompt_v3(lamp_review: str) -> str:
     return f"""
-Is the writer of the following review expressing anger?\
-The review is delimited with triple backticks. \
+Is the writer of the following review expressing anger?
+The review is delimited with triple backticks. 
 Give your answer as either yes or no.
 
 Review text: '''{lamp_review}'''
@@ -38,10 +38,10 @@ Identify the following items from the review text:
 - Item purchased by reviewer
 - Company that made the item
 
-The review is delimited with triple backticks. \
-Format your response as a JSON object with \
+The review is delimited with triple backticks.
+Format your response as a JSON object with
 "Item" and "Brand" as the keys. 
-If the information isn't present, use "unknown" \
+If the information isn't present, use "unknown"
 as the value.
 Make your response as short as possible.
   
@@ -57,10 +57,10 @@ Identify the following items from the review text:
 - Item purchased by reviewer
 - Company that made the item
 
-The review is delimited with triple backticks. \
-Format your response as a JSON object with \
+The review is delimited with triple backticks.
+Format your response as a JSON object with
 "Sentiment", "Anger", "Item" and "Brand" as the keys.
-If the information isn't present, use "unknown" \
+If the information isn't present, use "unknown"
 as the value.
 Make your response as short as possible.
 Format the Anger value as a boolean.
@@ -71,7 +71,7 @@ Review text: '''{lamp_review}'''
 # Version 6 - Infer 5 topics
 def prompt_v6(story: str) -> str:
     return f"""
-Determine five topics that are being discussed in the \
+Determine five topics that are being discussed in the
 following text, which is delimited by triple backticks.
 
 Make each item one or two words long. 
@@ -84,7 +84,7 @@ Text sample: '''{story}'''
 # Version 7 - Infer 5 topics
 def prompt_v7(story: str, topic_list: list) -> str:
     return f"""
-Determine whether each item in the following list of \
+Determine whether each item in the following list of
 topics is a topic in the text below, which
 is delimited with triple backticks.
 
